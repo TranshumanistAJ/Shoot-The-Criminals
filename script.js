1d87217a55ac2f3fbc05aab6bcadfa6e1ab2cb78
TranshumanistAJ <AJDeutschland103@hotmail.com>
2024-09-10 04:47:34 +0000

    add images into javascript

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const windows = document.querySelectorAll('.window');
 const scoreDisplay = document.getElementById('score');
 const doubleKillButton = document.getElementById('doubleKill');
 
+const images = {
+    criminals:
+    ['assets/images/criminal1.webp' ,
+        'assets/images/criminal2.png'
+    ],
+    innocents:
+    ['assets/images/innocent1.webp',
+        'assets/images/innocent2.webp'
+    ]
+
+};
+
 function startGame() {
     gameInterval = setInterval(() => {
         windows.forEach(window => {
